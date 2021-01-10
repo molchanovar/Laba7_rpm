de968a35bbbac034413c16186c0102abd6d379fe
molchanovar <64701071+molchanovar@users.noreply.github.com>
2021-01-10 19:54:13 +0300

    Update CreateRPM_script.sh

--- CreateRPM_script.sh
+++ CreateRPM_script.sh
@@ -42,6 +42,26 @@ gpgcheck=0
 enabled=1
 EOF
 
+cat > /etc/nginx/conf.d/default.conf << EOF
+server {
+    listen       80;
+    server_name  localhost;
+
+    location / {
+        root   /usr/share/nginx/html;
+        index  index.html index.htm;
+    }
+
+    error_page   500 502 503 504  /50x.html;
+    location = /50x.html {
+        root   /usr/share/nginx/html;
+    }
+    autoindex on;
+}
+
+EOF
+
+# Перезагрузка Nginx
 nginx -s reload
 nginx -t
 
